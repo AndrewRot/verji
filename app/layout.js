270de0e85698f89e27d9c7db4dfe1c7e77c406c5
andrewrot <andrewrottier95@gmail.com>
2026-03-04 00:56:50 -0800

    Add vercel speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Header from '@/components/Header';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata = {
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
